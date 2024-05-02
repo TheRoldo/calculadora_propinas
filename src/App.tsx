71cf8c3e1e29333fcbe2a2a5a67e7e31e3a70fab
unknown <nroldangaviria@gmail.com>
2024-05-02 15:11:00 -0500

    Calcular el consumo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import useOrder from "./hook/useOrder"
 
@@ -34,6 +35,10 @@ function App() {
           <OrderContents
             order={order}
             removeItem={removeItem}
+          />
+
+          <OrderTotals
+            order={order}
           />
         </div>
         
---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalProps = {
+    order: OrderItem[]
+}
+
+function OrderTotals({order}: OrderTotalProps) {
+
+    const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+  return (
+    <>
+        <div className=" space-y-3">
+
+            <h2 className=" font-black text-2xl">Totales y Propinas</h2>
+            <p>Subtotal a Pagar: {''} <span className="font-bold">{  formatCurrency(subTotalAmount) }</span></p>
+
+            <p>Propina {''} <span className="font-bold">$0</span></p>
+
+            <p>Total a Pagar: {''} <span className="font-bold">$0</span></p>
+
+        </div>
+
+        <button>
+
+        </button>
+    </>
+  )
+}
+
+export default OrderTotals
