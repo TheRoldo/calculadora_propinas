8e20ce98b59dbec51113ce417fdac79c61bf6b67
unknown <nroldangaviria@gmail.com>
2024-05-02 16:12:00 -0500

    Reiniciando la Orden

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import useOrder from "./hook/useOrder"
 
 function App() {
 
-  const { order, addItem, removeItem, tip, setTip } = useOrder()
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
 
   return (
     <>
@@ -33,19 +33,28 @@ function App() {
         </div>
 
         <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents
-            order={order}
-            removeItem={removeItem}
-          />
+          {order.length ? (
+            <>
+              <OrderContents
+                order={order}
+                removeItem={removeItem}
+            />
 
-          <TipPercentageForm 
-            setTip={setTip}
-          />
+            <TipPercentageForm 
+              setTip={setTip}
+              tip={tip}
+            />
 
-          <OrderTotals
-            order={order}
-            tip={tip}
-          />
+            <OrderTotals
+              order={order}
+              tip={tip}
+              placeOrder={placeOrder}
+            />
+            </>
+          ) : (
+            <p className=" text-center"> La orden Esta Vacia</p>
+          )}
+          
         </div>
         
       </main>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -4,14 +4,18 @@ import { formatCurrency } from "../helpers"
 
 type OrderTotalProps = {
     order: OrderItem[],
-    tip: number
+    tip: number,
+    placeOrder: () => void
 }
 
-function OrderTotals({order, tip}: OrderTotalProps) {
+function OrderTotals({order, tip, placeOrder}: OrderTotalProps) {
 
     const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
     const tipAmount = useMemo(() => subTotalAmount * tip, [tip, order])
+
+    const totalAmount = useMemo(() => subTotalAmount + tipAmount, [tip, order]) 
+
   return (
     <>
         <div className=" space-y-3">
@@ -21,12 +25,16 @@ function OrderTotals({order, tip}: OrderTotalProps) {
 
             <p>Propina {''} <span className="font-bold">{formatCurrency(tipAmount)}</span></p>
 
-            <p>Total a Pagar: {''} <span className="font-bold">$0</span></p>
+            <p>Total a Pagar: {''} <span className="font-bold">{formatCurrency(totalAmount)}</span></p>
 
         </div>
 
-        <button>
-
+        <button 
+            className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10" 
+            disabled={totalAmount === 0} 
+            onClick={placeOrder}
+        >
+            Guardar Orden
         </button>
     </>
   )
